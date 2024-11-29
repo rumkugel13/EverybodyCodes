fdb628f95446366e6655a57d1766e969c61e274f
rumkugel13 <rumkugel13@outlook.com>
2024-11-29 15:00:38 +0100

    optimize 1024/18/3

--- 2024-go/quest18.go
+++ 2024-go/quest18.go
@@ -16,41 +16,66 @@ func quest18() {
 	input := ReadLines("input/q18_p1.txt")
 	start := Point{1, 0}
 
-	result := q18_times(input, []Point{start}, q18_total_palmtrees(input))
+	result := q18_times(input, []Point{start}, len(q18_palmtrees(input)))
 	fmt.Println("Quest 18 Part 1:", result[len(result)-1])
 
 	input = ReadLines("input/q18_p2.txt")
 	starts := []Point{{1, 0}, {len(input) - 2, len(input[0]) - 1}}
-	result = q18_times(input, starts, q18_total_palmtrees(input))
+	result = q18_times(input, starts, len(q18_palmtrees(input)))
 	fmt.Println("Quest 18 Part 2:", result[len(result)-1])
 
 	input = ReadLines("input/q18_p3.txt")
-	minSum := math.MaxInt32
-	totalPalmtrees := q18_total_palmtrees(input)
-	for r, row := range input {
-		for c, cell := range row {
-			if cell == '.' {
-				times := q18_times(input, []Point{{r, c}}, totalPalmtrees)
-				sum := SumSlice(times)
-				if sum < minSum {
-					minSum = sum
-				}
-			}
+	palmtress := q18_palmtrees(input)
+	distances := map[Point]int{}
+	for _, tree := range palmtress {
+		times := q18_all_times(input, tree)
+		for point, time := range times {
+			distances[point] += time
 		}
 	}
+
+	minSum := math.MaxInt32
+	for point, distance := range distances {
+		if input[point.row][point.col] == '.' && distance < minSum {
+			minSum = distance
+		}
+	}
+
 	fmt.Println("Quest 18 Part 3:", minSum)
 }
 
-func q18_total_palmtrees(grid []string) int {
-	total := 0
-	for _, row := range grid {
-		for _, cell := range row {
+func q18_palmtrees(grid []string) []Point {
+	palmtrees := []Point{}
+	for r, row := range grid {
+		for c, cell := range row {
 			if cell == 'P' {
-				total++
+				palmtrees = append(palmtrees, Point{r, c})
 			}
 		}
 	}
-	return total
+	return palmtrees
+}
+
+func q18_all_times(grid []string, start Point) map[Point]int {
+	visited := map[Point]int{}
+	queue := []Point{start}
+	visited[start] = 0
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		distance := visited[current]
+		for _, dir := range Directions {
+			next := Point{current.row + dir.row, current.col + dir.col}
+			if !InsideGrid(grid, next) || grid[next.row][next.col] == '#' {
+				continue
+			}
+			if _, exists := visited[next]; !exists {
+				visited[next] = distance + 1
+				queue = append(queue, next)
+			}
+		}
+	}
+	return visited
 }
 
 func q18_times(grid []string, starts []Point, totalP int) []int {
@@ -63,16 +88,14 @@ func q18_times(grid []string, starts []Point, totalP int) []int {
 		queue = append(queue, start)
 	}
 
-	reachedP := 0
 	for len(queue) > 0 {
 		current := queue[0]
 		queue = queue[1:]
 
 		distance := visited[current]
 		if grid[current.row][current.col] == 'P' {
-			reachedP++
 			distances = append(distances, distance)
-			if reachedP == totalP {
+			if len(distances) == totalP {
 				return distances
 			}
 		}
